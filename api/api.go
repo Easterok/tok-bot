6c0f30d4f41a5dfeb81a5d893678d8759d32568e
easterok <beskrovnyk@gmail.com>
2024-01-01 14:09:55 +0300

    fix build, add echo

--- api/api.go
+++ api/api.go
@@ -1,7 +1,33 @@
 package api
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+)
 
 func Run() {
-	fmt.Println("todo: run api")
+	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	e.GET("/", hello)
+
+	e.Logger.Fatal(e.Start(fmt.Sprintf(":%s", os.Getenv("API_PORT"))))
+}
+
+func hello(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello, World!")
 }
